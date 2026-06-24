d5691e3b05c0e7102f8d29c05c7c7283f0601c35
lavenly <135024283+lavenly@users.noreply.github.com>
2026-06-23 19:47:30 -0700

    j2lte: set device name

--- configs/bluetooth/bdroid_buildcfg.h
+++ configs/bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy J2"
+#define BTM_DEF_LOCAL_NAME   "Galaxy J2"
 
 /* Inherit from universal3475-common */
 #include "../../universal3475-common/configs/bluetooth/bdroid_buildcfg.h"
